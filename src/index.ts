d32e01f6f6322c29b4a79e738762c7750905ee50
Mansur <mansur.khusnutdinov@underhood.dev>
2023-10-31 00:35:43 +0300

    router (in progress)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Block from './utils/Block';
+// import Block from './utils/Block';
 import { Main } from './pages/Main';
 import { Login } from './pages/Login';
 import { NotFound } from './pages/NotFound';
@@ -9,25 +9,72 @@ import { ChangePassword } from './pages/ChangePassword';
 import { ServerError } from './pages/ServerError';
 import { Chat } from './pages/Chat';
 import './styles/index.scss';
+import router from './utils/Router';
 
-const ROUTES: Record<string, Block> = {
-  '/': new Main(),
-  '/main': new Main(),
-  '/404': new NotFound(),
-  '/login': new Login(),
-  '/signup': new SignUp(),
-  '/editProfile': new EditProfile(),
-  '/profile': new Profile(),
-  '/changePassword': new ChangePassword(),
-  '/serverError': new ServerError(),
-  '/chat': new Chat(),
-};
+enum Routes {
+  MainPage = '/main',
+  LoginPage = '/login',
+  NotFoundPage = '/notFound',
+  SignUpPage = '/signup',
+  EditProfilePage = '/editProfile',
+  ProfilePage = '/profile',
+  ChangePasswordPage = '/changePassword',
+  ServerErrorPage = '/serverError',
+  ChatPage = '/chat'
+}
+
+// const ROUTES: Record<string, Block> = {
+//   '/': new Main(),
+//   '/main': new Main(),
+//   '/404': new NotFound(),
+//   '/login': new Login(),
+//   '/signup': new SignUp(),
+//   '/editProfile': new EditProfile(),
+//   '/profile': new Profile(),
+//   '/changePassword': new ChangePassword(),
+//   '/serverError': new ServerError(),
+//   '/chat': new Chat(),
+// };
 
 window.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('app')!;
+  router
+    .use(Routes.MainPage, Main)
+    .use(Routes.LoginPage, Login)
+    .use(Routes.NotFoundPage, NotFound)
+    .use(Routes.SignUpPage, SignUp)
+    .use(Routes.EditProfilePage, EditProfile)
+    .use(Routes.ProfilePage, Profile)
+    .use(Routes.ChangePasswordPage, ChangePassword)
+    .use(Routes.ServerErrorPage, ServerError)
+    .use(Routes.ChatPage, Chat);
 
-  const component = ROUTES[window.location.pathname] || new NotFound();
+  let isProtectedRoute = true;
 
-  root.append(component.element!);
-  component.dispatchComponentDidMount();
+  switch (window.location.pathname) {
+    case Routes.LoginPage:
+    case Routes.SignUpPage:
+      isProtectedRoute = false;
+      break;
+  }
+
+  try {
+    router.start();
+
+    // if (!isProtectedRoute) {
+    //   router.go(Routes.ProfilePage);
+    // }
+  } catch (e) {
+    // console.log(e, 'Here');
+    router.start();
+
+    if (isProtectedRoute) {
+      router.go(Routes.LoginPage);
+    }
+  }
+  // const root = document.getElementById('app')!;
+  //
+  // const component = ROUTES[window.location.pathname] || new NotFound();
+  //
+  // root.append(component.element!);
+  // component.dispatchComponentDidMount();
 });
